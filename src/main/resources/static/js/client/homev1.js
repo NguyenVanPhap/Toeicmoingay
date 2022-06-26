43804438bb132740363b20bb2988cf75e2f2a409
NguyenVanPhap <nguyenvanphap013@gmail.com>
2022-06-26 11:24:26 +0700

    ok

---
+++ src/main/resources/static/js/client/homev1.js
@@ -0,0 +1,58 @@
+
+
+
+$(document).ready(function() {
+
+
+	var domain = location.protocol + '//' + location.host;
+	//default. load all object baiGrammar
+	window.onload = function() {
+
+		loadAllGrammar(1);
+
+	};
+
+
+
+
+	function loadAllGrammar(pageinput) {
+
+		var namekey = "";
+		var datainput = { keyWord: namekey, page: pageinput };
+		console.log(datainput);
+		$.ajax({
+			type: "POST",
+			contentType: "application/json",
+			url: domain + "/api/admin/grammar/getlist",
+			data: JSON.stringify(datainput),
+			success: function(data) {
+				console.log(data);
+				var trHTML = "";
+				var index = 0;
+				$.each(data.object.content, function(i, objgrammar) {
+					if (index < 7) {
+						indexval = index + 1;
+						var idname = "#post" + indexval;
+
+						$(idname).text(objgrammar.grammarName);
+						$(idname).attr("href", "/detailGram?idGram=" + objgrammar.grammarId)
+						index++;
+					}
+
+				});
+
+
+				//$('#tableExam').append(trHTML);
+				$('tbody').html(trHTML);
+
+			}, error: function(e) {
+				alert("error");
+				console.log("ERROR: ", e);
+			}
+		});
+
+
+	}
+
+
+});
